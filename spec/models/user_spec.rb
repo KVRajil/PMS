1ee1b31b663f08cbd034ad755046393e08adb064
KVRajil <rajil.kv@byjus.com>
2022-11-04 18:55:03 +0530

    add user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+
+  context 'validation test' do
+    it 'ensures username presence' do
+      user.username = nil
+      expect(user).to_not be_valid
+    end
+
+    it 'ensures email presence' do
+      user.email = nil
+      expect(user).to_not be_valid
+    end
+
+    it 'ensures password presence' do
+      user.password = nil
+      expect(password).to_not be_valid
+    end
+
+    it 'ensures username uniqueness' do
+      new_user = user.dup
+      new_user = new_user.email = 'valid_uniq@test.com'
+      expect(new_user).to_not be_valid
+    end
+
+    it 'ensures email uniqueness' do
+      new_user = user.dup
+      new_user = new_user.username = 'test_uniq'
+      expect(new_user).to_not be_valid
+    end
+  end
 end
