99086b0d03f2c8b5204f6c6dd5f08efb8cfd1937
KVRajil <rajil.kv@byjus.com>
2022-11-04 18:54:52 +0530

    add project model spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,7 +1,24 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+  let!(:project) { create(:project, title: 'Sample Project 1', description: 'Test description', user: user) }
+
+
+  context 'validation test' do
+    it 'ensures title presence' do
+      project.title = nil
+      expect(project).to_not be_valid
+    end
+
+    it 'ensures user_id presence' do
+      project.user_id = nil
+      expect(project).to_not be_valid
+    end
+
+    it 'ensures title length should not be too long' do
+      project.title = [*('AA'..'GG')].sample(150).join('')
+      expect(project).to_not be_valid
+    end
+  end
 end
