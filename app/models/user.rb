a5d7073627a3e25990202fe8de977663bdd3ffe0
KVRajil <rajil.kv@byjus.com>
2022-11-05 18:23:50 +0530

    add user ordered scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ApplicationRecord
   has_many :projects, inverse_of: :user, dependent: nil
   has_many :assigned_tasks,  class_name: 'Task', foreign_key: :assignee_id, inverse_of: :assignee, dependent: :delete_all
   has_many :reporting_tasks, class_name: 'Task', foreign_key: :reporter_id, inverse_of: :reporter, dependent: :delete_all
+
+  scope :ordered, -> { order(id: :asc) }
 end
