7a670d85957171e3df213acb75e28aafef51a603
KVRajil <rajil.kv@byjus.com>
2022-11-05 14:48:43 +0530

    project controller rubcop fixes

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,12 +1,12 @@
 module Api
   module V1
-    class Api::V1::ProjectsController < Api::V1::ApiController
+    class ProjectsController < ApiController
       before_action :authenticate_user!
-      before_action :set_project, only: %i[show edit update destroy]
+      before_action :set_project, only: %i[show update destroy]
 
       api :GET, 'api/v1/projects', 'List all projects'
-      param :page,     String, desc: 'Page number for pagination',      required: false
-      param :per_page, String, desc: 'Records per page for pagination', required: false
+      param :page,     :number, desc: 'Page number for pagination',      required: false
+      param :per_page, :number, desc: 'Records per page for pagination', required: false
       def index
         @projects = Project.includes(:user).ordered.page(params[:page]).per(params[:per_page])
       end
@@ -14,16 +14,16 @@ module Api
       api :GET, 'api/v1/projects/:id', 'Get project details'
       def show; end
 
-      api :POST, 'api/v1/cms/projects', 'Create a new project '
+      api :POST, 'api/v1/projects', 'Create a new project '
       param :title, String, desc: 'Project title', required: true
-      param :description, String, desc: 'Project description', required: true
+      param :description, String, desc: 'Project description', required: false
       def create
         @project = current_user.projects.create!(project_params)
 
         head :ok
       end
 
-      api :PUT, 'api/v1/cms/projects', 'Update an existing project'
+      api :PUT, 'api/v1/projects', 'Update an existing project'
       param :title, String, desc: 'Project title', required: false
       param :description, String, desc: 'Project description', required: false
       def update
@@ -31,7 +31,7 @@ module Api
 
         head :ok
       end
-      api :DELETE, 'api/v1/cms/projects/:', 'Delete an existing project'
+      api :DELETE, 'api/v1/projects/:id', 'Delete an existing project'
       def destroy
         @project.destroy!
 
